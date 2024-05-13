e147b8be73822f69bc8903a9e80dba5063e9a32e
arumullasivakrishnacodes <arumullasivakrishna@outlook.com>
2024-05-13 12:15:10 +0530

    c

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use('/images',express.static('Upload/Images'));
 app.post('/upload', upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
+        image_url: `https://ecom-api-tau.vercel.app/images/${req.file.filename}`
     });
 });
 
